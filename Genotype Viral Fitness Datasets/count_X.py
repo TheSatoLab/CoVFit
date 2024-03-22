b9a9247d367c28f0685e2e11657daef2a5621c5b
AdamInTokyo <135804633+AdamInTokyo@users.noreply.github.com>
2024-03-22 10:40:31 +0900

    Update count_X.py
    Removed broken dependency

--- Genotype Viral Fitness Datasets/count_X.py
+++ Genotype Viral Fitness Datasets/count_X.py
@@ -3,17 +3,23 @@
 '''This script takes the nextclade S gene protein translation file and counts the number of Xs and stop codons in the sequence'''
 
 import sys
-from adam_utils.bio import fasta
 
 argvs = sys.argv
 
 f = open(argvs[1])
 
-#max_N_prop = 0.01 # This setting and filtering is actually in the R script
+max_N_prop = 0.01
 
-seq_d = fasta.to_dict(f,unique=True)
+seq_d = {}
+for line in f:
+  line = line.strip()
+  if '>' in line:
+    name = line.replace(">","")
+    seq_d[name] = ""
+  else:
+    seq_d[name] += line
 
-#total_len = len(seq_d[name])
+total_len = len(seq_d[name])
 
 print("name\tlength\tcount_N\tcount_stop")
 for name in seq_d:
